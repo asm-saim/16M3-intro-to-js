f16718cc49d043ea8892064537c79196b5750a9d
asm-saim <saim777m@gmail.com>
2026-05-23 15:08:06 +0600

    operation and shorthand implemented

---
+++ operation.js
@@ -0,0 +1,38 @@
+// operator -> + - * / = ** %(modulus)
+// operand -> num1 num2 
+// operation -> x + y = 45;
+
+var orange = 344;
+var berry = 569;
+total =orange + berry;
+
+console.log(total);
+
+// 
+var x= 45;
+var y= 34;
+sub= x - y;
+
+console.log(x+y);
+
+// Multiply
+var value1 = 34;
+var value2 = 56;
+mul= value1 * value2;
+console.log(value1+value2);
+
+// Division
+var bat = 34;
+var ball = 23;
+
+div=ball/ball;
+
+console.log(div);
+
+// Modulus
+var bat = 34;
+var ball = 23;
+
+mod=bat % ball;
+
+console.log(mod);
---
+++ shortHand.js
@@ -0,0 +1,19 @@
+// Shorthand:
+
+var x=20;
+// var num= num + 30;
+x += 30;
+console.log(x);
+
+
+// Subtract
+
+var orange = 300;
+ orange-=50;
+ console.log(orange);
+
+ // Division
+
+var pineapple = 100;
+ pineapple/=2;
+ console.log(pineapple);
